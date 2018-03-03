f1cf5571b3de490d1f1d528a928c17a9d9562160
Kilian Ganz <kiliganz@web.de>
2018-03-03 14:46:30 +0100

    First_Commit_SwipeView

---
+++ InfoPhilo2018/app/src/main/java/ur/moralmachine_altmann_ganz_schnell/GameActivity.java
@@ -0,0 +1,13 @@
+package ur.moralmachine_altmann_ganz_schnell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
